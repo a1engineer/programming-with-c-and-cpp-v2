c1f6c66ae5b3cb1a34ba2d053be5e07e98042ada
Pankaj Kumar <37345253+a1engineer@users.noreply.github.com>
2021-12-16 09:34:30 +0530

    Update variables-data-types-literals.c

--- m1-getting-started-with-programming-in-c/t6-tokens/variables-data-types-literals.c
+++ m1-getting-started-with-programming-in-c/t6-tokens/variables-data-types-literals.c
@@ -1,22 +1,10 @@
-/*
- Author: 	Internshala
- Module: 	Getting Started with Programming in C
- Topic: 	Tokens
-*/
-
-#include <stdio.h>
-
-int main() {
-
-	// pizza name is Farm House
-	// size is M
-	// person it serves is 2
-	// price including tax is 365.8
-
-	char name[15] = "Farm House";
-	char size = 'M';
-	int personItServes = 2;
-	float retailPrice = 365.8;
-
+#include<stdio.h>
+int main()
+{
+	char name[10] = "India";
+	char currency = '$';
+	float gdp = 2.2;
+	int year = 2015;
+	printf("%s", name);
 	return 0;
 }
